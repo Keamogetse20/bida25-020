d3dc16b48c2154b9544351086d10d8c6d9506eaa
Keamogetse Othata <bida25-020@thuto.bac.ac.bw>
2026-04-24 01:47:30 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,46 @@
 console.log("JavaScript is connected!");
+let selectedCategory = "";
+let selectedColor = "";
+
+function toggleMenu() {
+  const menu = document.getElementById("dropdownMenu");
+  menu.style.display = menu.style.display === "flex" ? "none" : "flex";
+}
+
+function filterCategory(category) {
+  selectedCategory = category;
+  selectedColor = "";
+  document.getElementById("dropdownMenu").style.display = "none";
+  applyFilters();
+}
+
+function filterColor(color) {
+  selectedColor = color;
+  applyFilters();
+}
+
+function clearColor() {
+  selectedColor = "";
+  applyFilters();
+}
+
+function applyFilters() {
+  const products = document.querySelectorAll(".product");
+
+  products.forEach(product => {
+    const category = product.dataset.category;
+    const color = product.dataset.color;
+
+    const matchCategory =
+      !selectedCategory || category === selectedCategory;
+
+    const matchColor =
+      !selectedColor || color === selectedColor;
+
+    product.style.display =
+      matchCategory && matchColor ? "block" : "none";
+  });
+}
 const backToTopButton = document.getElementById('back-to-top');
 
 window.addEventListener('scroll', function() {
