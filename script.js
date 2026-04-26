0edd4deacc085cc4025b07cdb6b27425f0d69777
Keamogetse Othata <bida25-020@thuto.bac.ac.bw>
2026-04-26 19:26:50 +0200

    Update script.js

--- script.js
+++ script.js
@@ -5,3 +5,8 @@ const dropdownMenu = document.getElementById("dropdown-menu");
 menuToggle.addEventListener("click", () => {
     dropdownMenu.classList.toggle("active");
 });
+document.addEventListener("click", (e) => {
+    if (!menuToggle.contains(e.target) && !dropdownMenu.contains(e.target)) {
+        dropdownMenu.classList.remove("active");
+    }
+});
