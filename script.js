ac86be4ed9ac4261a869b8f72055ea2940ad9db4
Keamogetse Othata <bida25-020@thuto.bac.ac.bw>
2026-04-26 21:08:26 +0200

    Update script.js

--- script.js
+++ script.js
@@ -19,3 +19,44 @@ document.addEventListener("DOMContentLoaded", () => {
         }
     });
 });
+const slides = document.querySelectorAll(".slide");
+const nextBtn = document.getElementById("nextBtn");
+const prevBtn = document.getElementById("prevBtn");
+
+let currentSlide = 0;
+
+// SHOW SLIDE
+function showSlide(index) {
+    slides.forEach((slide, i) => {
+        slide.classList.remove("active");
+        if (i === index) {
+            slide.classList.add("active");
+        }
+    });
+}
+
+// NEXT
+nextBtn.addEventListener("click", () => {
+    currentSlide = (currentSlide + 1) % slides.length;
+    showSlide(currentSlide);
+});
+
+// PREVIOUS
+prevBtn.addEventListener("click", () => {
+    currentSlide = (currentSlide - 1 + slides.length) % slides.length;
+    showSlide(currentSlide);
+});
+
+// AUTO SLIDE
+setInterval(() => {
+    currentSlide = (currentSlide + 1) % slides.length;
+    showSlide(currentSlide);
+}, 3000); // every 3 seconds
+const categories = document.querySelectorAll(".dropdown-menu li");
+const slideshow = document.querySelector(".slideshow-container");
+
+categories.forEach(item => {
+    item.addEventListener("click", () => {
+        slideshow.style.display = "none";
+    });
+});
