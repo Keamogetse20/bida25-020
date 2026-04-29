c8fd159b415233d36434a1c369c8a23b236a4de4
Keamogetse Othata <bida25-020@thuto.bac.ac.bw>
2026-04-29 23:33:20 +0200

    Update script.js

--- script.js
+++ script.js
@@ -54,3 +54,67 @@ document.addEventListener("DOMContentLoaded", () => {
     });
 
 });
+
+const categories = document.querySelectorAll("#dropdown-menu li");
+const slideshow = document.querySelector(".slideshow-container");
+const productsSection = document.querySelector(".products-section");
+
+categories.forEach(item => {
+    item.addEventListener("click", () => {
+
+        const selectedCategory = item.textContent.toLowerCase();
+
+        slideshow.style.display = "none";
+        productsSection.style.display = "flex";
+
+        document.querySelectorAll(".product-card").forEach(card => {
+            if (card.dataset.category === selectedCategory) {
+                card.style.display = "block";
+            } else {
+                card.style.display = "none";
+            }
+        });
+
+    });
+});
+const colors = document.querySelectorAll(".color-sidebar li");
+
+colors.forEach(color => {
+    color.addEventListener("click", () => {
+
+        const selectedColor = color.dataset.color;
+
+        document.querySelectorAll(".product-card").forEach(card => {
+
+            if (selectedColor === "all" || card.dataset.color === selectedColor) {
+                card.style.display = "block";
+            } else {
+                card.style.display = "none";
+            }
+
+        });
+
+    });
+});
+const products = document.querySelectorAll(".product-card");
+
+products.forEach(product => {
+    product.addEventListener("click", () => {
+
+        const sizes = product.querySelector(".sizes");
+
+        sizes.style.display =
+            sizes.style.display === "block" ? "none" : "block";
+    });
+});
+document.querySelectorAll(".sizes span").forEach(size => {
+    size.addEventListener("click", (e) => {
+        e.stopPropagation(); // stops product click
+
+        document.querySelectorAll(".sizes span").forEach(s => {
+            s.style.background = "white";
+        });
+
+        size.style.background = "#d9a5a5";
+    });
+});
